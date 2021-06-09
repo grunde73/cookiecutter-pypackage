f7d3a8d9732f1afaa9a1dfdbe3700940f5cbb3bf
Grunde Loevoll <grunde@fiskekroken.org>
2021-06-09 15:06:43 +0200

    added logging to cli

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/cli.py
@@ -1,15 +1,21 @@
 """Console script for {{cookiecutter.project_slug}}."""
 
+import logging
 {%- if cookiecutter.command_line_interface|lower == 'argparse' %}
 import argparse
 {%- endif %}
 import sys
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
 import click
+import click_log
 {%- endif %}
+
+
+logger = logging.getLogger(__name__)
 
 {% if cookiecutter.command_line_interface|lower == 'click' %}
 @click.command()
+@click_log.basic_config(logger)
 def main(args=None):
     """Console script for {{cookiecutter.project_slug}}."""
     click.echo("Replace this message by putting your code into "
